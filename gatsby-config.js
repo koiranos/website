a701bbf707716296d2bf481f0ee2bc89f9dbd65a
Joseph Rex <joerexko@gmail.com>
2019-11-18 21:37:41 -0600

    Add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `StyleQ`,
+    description: `Bringing stylists to you quicker than you ever imagined`,
+    author: `@josephrexme`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,6 +26,15 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
+    },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        generateMatchPathRewrites: true,
+        mergeLinkHeaders: true,
+        mergeSecurityHeaders: true,
+        mergeCachingHeaders: true
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
