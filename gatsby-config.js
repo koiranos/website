ace4f81950a700f551e2eb4aaf6da56910e3dc5b
Joseph Rex <joerexko@gmail.com>
2019-12-05 23:07:20 -0600

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,12 @@ module.exports = {
         mergeCachingHeaders: true
       }
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-154163112-1",
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
