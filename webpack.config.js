81cd2ddcf5d1d9060253adbf5f55b7b8f3ff1e17
Joseph Rex <joerexko@gmail.com>
2020-03-23 18:12:43 -0500

    Set up babel, editorconfig, and complete webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,48 @@
-require('dotenv').config();
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTemplate = require('html-webpack-template');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-console.log(process.env.NODE_ENV);
-module.exports = {
-  mode: 'development',
-  entry: {},
-  plugins: [],
-  output: {
-    filename: '',
-  }
+module.exports = env => {
+  return {
+    mode: env.NODE_ENV,
+    entry: {
+      app: './src/index.js'
+    },
+    devServer: {
+      contentBase: './public',
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          use: ['babel-loader'], // TODO: Add eslint-loader
+          exclude: /node_modules/,
+        },
+      ]
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+    plugins: [
+      new ExtractTextPlugin({ filename: 'app.css', allChunks: true }),
+      new HtmlWebpackPlugin({
+        title: 'StyleQ - Find professional stylists',
+        template: HtmlWebpackTemplate,
+        inject: false,
+        hash: true,
+        mobile: true,
+        // Mount ids for main app and modal portal
+        appMountIds: ['app', 'modal']
+      }),
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'public'),
+      publicPath: '/',
+    },
+  };
 };
